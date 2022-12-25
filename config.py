f050cbc4f2edc1a005709facde44af717e38869f
arturbaccarin <baccarin.artur@gmail.com>
2022-12-25 07:14:34 -0300

    feat: add env var config

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
+import os
+DB_HOST = os.environ['POSTGRES_HOST']
+DB_NAME = os.environ['POSTGRES_DB']
+DB_USER = os.environ['POSTGRES_USER']
+DB_PASS = os.environ['POSTGRES_PASSWORD']
+
+
 class DevelopmentConfig:
     TESTING = False
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 
 
 class TestingConfig:
